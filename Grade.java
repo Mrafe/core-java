350ad2d99c3eb08e71d1a350273aa203ccb27be8
Mohammad Rafe <mrafe1410@gmail.com>
2022-02-09 15:29:32 +0530

    task 9th

---
+++ Grade.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Grade {
+    static char grade(int marks) {
+        if (marks >= 90)
+            return 'A';
+        else if (marks >= 80)
+            return 'B';
+        else if (marks >= 70)
+            return 'C';
+        else if (marks >= 60)
+            return 'D';
+        else
+            return 'F';
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the marks");
+        int phy = scanner.nextInt();
+        int chem = scanner.nextInt();
+        int maths = scanner.nextInt();
+        int marks = phy + chem + maths;
+        System.out.println("The grade is : " + grade(marks));
+        scanner.close();
+    }
+}
---
+++ Triangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Triangle {
+    static String CheckType(int side1, int side2, int side3) {
+        if (side1 == side2 && side2 == side3)
+            return "equilateral triangle";
+        else if (side1 == side2 || side2 == side3 || side3 == side1)
+            return "isoceless triangle";
+        else
+            return "scalene triangle";
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the three sides of the triangle");
+        int side1 = scanner.nextInt();
+        int side2 = scanner.nextInt();
+        int side3 = scanner.nextInt();
+        System.out.println("It is a " + CheckType(side1, side2, side3));
+        scanner.close();
+    }
+}
